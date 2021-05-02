b40eccee0060cbc0b1a16504c5f8d5c624974e7d
fabio-pv <fabiopv1994@gmail.com>
2021-05-02 13:13:37 -0300

    implementado os campos do meio de pagamento

---
+++ src/Components/TextFieldDefaultComponent/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import {TextField} from "@material-ui/core";
+
+
+export const TextFieldDefaultStyled = styled(TextField)`
+  background: #C4C4C4;
+  border: none;
+  margin: 0;
+`;
--- src/Screens/Sale/ContentPaymentSale.js
+++ src/Screens/Sale/ContentPaymentSale.js
@@ -1,13 +1,77 @@
 import React, {Component} from 'react';
 import CardComponent from "../../Components/CardComponent/CardComponent";
+import {Box, Grid, Typography} from "@material-ui/core";
+import TextFieldDefaultWithGridComponent
+    from "../../Components/TextFieldDefaultWithGridComponent/TextFieldDefaultWithGridComponent";
+import SpacerComponent from "../../Components/SpacerComponent/SpacerComponent";
+import {ReactComponent as VisaLogo} from "../../Assets/visa-color_large.svg";
+import {ReactComponent as AmericanLogo} from "../../Assets/americanexpress-color-large.svg";
+import {ReactComponent as MastercardLogo} from "../../Assets/mastercard-color_large.svg";
 
 class ContentPaymentSale extends Component {
+
+    handleChange(event) {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    }
+
+
     render() {
         return (
             <CardComponent desktop={8}
                            mobile={12}
                            title={'Dados da forma de pagamento'}>
-                <h5>Dados da forma de pagamento</h5>
+                <Grid container={true}
+                      spacing={3}>
+                    <TextFieldDefaultWithGridComponent desktop={4}
+                                                       mobile={12}
+                                                       label={'Número do cartão'}
+                                                       name={'card_number'}
+                                                       type={'number'}
+                                                       size={'small'}
+                                                       erros={this.props.erros}
+                                                       onChange={(event) => this.handleChange(event)}/>
+                    <TextFieldDefaultWithGridComponent desktop={3}
+                                                       mobile={12}
+                                                       label={'Data de expiração'}
+                                                       name={'expiration_date'}
+                                                       type={'text'}
+                                                       size={'small'}
+                                                       erros={this.props.erros}
+                                                       onChange={(event) => this.handleChange(event)}/>
+                </Grid>
+                <Grid container={true}
+                      spacing={3}>
+                    <TextFieldDefaultWithGridComponent desktop={4}
+                                                       mobile={12}
+                                                       label={'Titular do cartão'}
+                                                       name={'card_holder'}
+                                                       type={'text'}
+                                                       size={'small'}
+                                                       erros={this.props.erros}
+                                                       onChange={(event) => this.handleChange(event)}/>
+                    <TextFieldDefaultWithGridComponent desktop={3}
+                                                       mobile={12}
+                                                       label={'CVV'}
+                                                       name={'cvv'}
+                                                       type={'number'}
+                                                       size={'small'}
+                                                       erros={this.props.erros}
+                                                       onChange={(event) => this.handleChange(event)}/>
+                </Grid>
+                <SpacerComponent height={30}/>
+                <div>
+                    <Typography variant={'body1'}>
+                        Bandeiras aceitas
+                    </Typography>
+                    <Box flexDirection={'row'}
+                         justifyContent={'flex-start'}
+                         alignItems={'center'}>
+                        <VisaLogo style={{marginRight: 10}}/>
+                        <MastercardLogo style={{marginRight: 10}}/>
+                        <AmericanLogo/>
+                    </Box>
+                </div>
             </CardComponent>
         );
     }
